6de6d18f26a8b8138a36ff7c918a74feacdd9ab7
Jack <jack9966qk@gmail.com>
2026-02-16 01:13:14 +0800

    Separate more clearly public and private API in the renderer package. Update app to use package dependency.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,2 @@
 export * from "./api.js";
-export * from "./auto-annotation.js";
-export * from "./primitives.js";
-export * from "./renderer.js";
-export { getBranchLineAt, getChartElementAt, getNoteAt } from "./renderer.js";
-export * from "./tja-parser.js";
+export * as Private from "./internal.js";
---
+++ src/internal.ts
@@ -0,0 +1,4 @@
+export * from "./auto-annotation.js";
+export * from "./primitives.js";
+export * from "./renderer.js";
+export * from "./tja-parser.js";
